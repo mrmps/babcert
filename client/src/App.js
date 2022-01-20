0b97bd4f6673d6f9530e0d77bc95ab9c1b70a67f
Michael Ryaboy <sfr.michael@yahoo.com>
2022-01-20 15:01:00 -0800

    first

--- client/src/App.js
+++ client/src/App.js
@@ -7,10 +7,6 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">React Express Starter</h1>
-        </header>
         <Customers />
       </div>
     );
--- client/src/components/customers.js
+++ client/src/components/customers.js
@@ -1,32 +1,66 @@
-import React, { Component } from 'react';
-import './customers.css';
+import React, { useState } from 'react';
+//exported customers component
+export default function Customers() {
 
-class Customers extends Component {
-  constructor() {
-    super();
-    this.state = {
-      customers: []
-    };
-  }
 
-  componentDidMount() {
-    fetch('/api/customers')
+  //   Retrieve Data from (https://randomuser.me/api) 
+  // Fetch a new user every time you click a button and append it to local state 
+  // Display (userImg and name) in an array
+  // Handle state loading and state dependencies as you like 
+  // Handle styling as you like 
+  // Handle component breakdown as you like 
+  // *Add Remove Button that removes the user info from the array list 
+  // Be creative with data if you have time! 
+  const [users, setUsers] = React.useState([]);
+  const [loading, setLoading] = React.useState(false);
+
+  const handleClick = () => {
+    setLoading(true);
+    fetch("https://randomuser.me/api/")
       .then(res => res.json())
-      .then(customers => this.setState({customers}, () => console.log('Customers fetched...', customers)));
-  }
+      .then(data => {
+        setUsers([...users, data.results[0]]);
+        setLoading(false);
+      });
+  };
 
-  render() {
-    return (
-      <div>
-        <h2>Customers</h2>
-        <ul>
-        {this.state.customers.map(customer => 
-          <li key={customer.id}>{customer.firstName} {customer.lastName}</li>
-        )}
-        </ul>
-      </div>
-    );
-  }
-}
+  const handleRemove = (index) => {
+    setUsers(users.filter((user, i) => i !== index));
+  };
 
-export default Customers;
+
+  return (
+    <div>
+      <h1>Get a Random User</h1>
+      <button style={{"marginBottom": 10}} onClick={handleClick}>Fetch New User</button>
+      {loading ? (
+        <div>Loading...</div>
+      ) : (
+        <div>
+          {users.map(user => (
+            <div key={user}>
+              <img src={user.picture.large} alt={user.name.first} />
+              <h2 style={{"marginBottom": 10}}>{user.name.first + " " + user.name.last}</h2>
+              <button style={{"marginBottom": 10}} onClick={() => handleRemove(users.indexOf(user))}>Remove</button>
+            </div>
+          ))}
+        </div>
+      )}
+    </div>
+  );
+};
+
+
+
+  // const listItems = users.map((user) =>
+  // <li>{user}</li>
+  // );
+  //call the random image api and add the image to the list
+  // handleClick() {
+  //   fetch("https://dog.ceo/api/breeds/image/random")
+  //     .then((response) => response.json())
+  //     .then((data) => {
+  //       setUsers([...users, data.message]);
+  //     }
+  //     );
+  // }
